b67abc8d7e16522d8b25475445d8873a594c0b9c
meteorx <imeteorjs@gmail.com>
2017-08-30 14:54:26 +0800

    fix

--- www/main.js
+++ www/main.js
@@ -14,13 +14,12 @@ Vue.use(MintUI);
 Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
 
 // 开发环境配置
-Vue.prototype.$api = "http://192.168.0.222:8080" //api地址
-Vue.prototype.$export = "http://192.168.0.222:8080" //导出地址前缀
-Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
+//Vue.prototype.$api = "http://192.168.0.222:8080" //api地址
+//Vue.prototype.$export = "http://192.168.0.222:8080" //导出地址前缀
 
 // 生产环境配置
-//Vue.prototype.$api = "http://116.62.68.26:8080" //api地址
-//Vue.prototype.$export = "http://wx.urskongjian.com:8080" //导出地址前缀
+Vue.prototype.$api = "http://116.62.68.26:8080" //api地址
+Vue.prototype.$export = "http://wx.urskongjian.com:8080" //导出地址前缀
 
 //Vue.config.debug = true;// 开启debug模式
 Vue.config.debug = true;// 开启debug模式
